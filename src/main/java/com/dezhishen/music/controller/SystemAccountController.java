9a39c1ce36d67c21875e0f6f12db106803531d6d
dezhiShen <1179551960@qq.com>
2021-01-15 15:06:46 +0800

    feat(account):增加登录接口

--- src/main/java/com/dezhishen/music/controller/SystemAccountController.java
+++ src/main/java/com/dezhishen/music/controller/SystemAccountController.java
@@ -3,12 +3,11 @@ package com.dezhishen.music.controller;
 import com.dezhishen.music.base.BaseController;
 import com.dezhishen.music.base.RespEntity;
 import com.dezhishen.music.domain.SystemAccount;
+import com.dezhishen.music.dto.LoginRequest;
+import com.dezhishen.music.dto.LoginResult;
 import com.dezhishen.music.service.SystemAccountService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/system/account")
@@ -19,6 +18,14 @@ public class SystemAccountController extends BaseController {
     @PostMapping("/register")
     public RespEntity<Boolean> register(@RequestBody SystemAccount account) {
         return success(systemAccountService.registerAccount(account));
+    }
+
+    @PostMapping("/login")
+    public RespEntity<LoginResult> login(@RequestParam String loginName, @RequestParam String password) {
+        LoginRequest request = new LoginRequest();
+        request.setLoginName(loginName);
+        request.setPassword(password);
+        return success(systemAccountService.login(request));
     }
 
 
