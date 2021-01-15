574b11d02636a721d94d927f377f4960f15e6dff
dezhiShen <1179551960@qq.com>
2021-01-15 14:43:45 +0800

    mod(账号):账号注册修改

--- src/main/java/com/dezhishen/music/service/SystemAccountService.java
+++ src/main/java/com/dezhishen/music/service/SystemAccountService.java
@@ -2,7 +2,12 @@ package com.dezhishen.music.service;
 
 import com.dezhishen.music.domain.SystemAccount;
 
-public interface SystemAccountService extends IBaseService<SystemAccount>{
+public interface SystemAccountService extends IBaseService<SystemAccount> {
+    /**
+     * 注册账号
+     *
+     * @param account
+     * @return
+     */
     boolean registerAccount(SystemAccount account);
-    void insertAsync(SystemAccount account);
 }
--- src/main/java/com/dezhishen/music/service/impl/SystemAccountServiceImpl.java
+++ src/main/java/com/dezhishen/music/service/impl/SystemAccountServiceImpl.java
@@ -1,28 +1,26 @@
 package com.dezhishen.music.service.impl;
 
 import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.dezhishen.music.domain.SystemAccount;
 import com.dezhishen.music.domain.SystemUser;
 import com.dezhishen.music.exception.MusicException;
 import com.dezhishen.music.mapper.SystemAccountMapper;
-import com.dezhishen.music.mapper.SystemUserMapper;
 import com.dezhishen.music.service.SystemAccountService;
 import com.dezhishen.music.service.SystemUserService;
 import com.dezhishen.music.uitl.PasswordUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StringUtils;
 
-import java.util.List;
-
+// change 补充注解
+@Service
 public class SystemAccountServiceImpl extends AbstractServiceImpl<SystemAccount> implements SystemAccountService {
 
     @Autowired
     private SystemAccountMapper systemAccountMapper;
-
-    @Autowired
-    private SystemUserMapper systemUserMapper;
 
     @Autowired
     private SystemUserService systemUserService;
@@ -34,40 +32,48 @@ public class SystemAccountServiceImpl extends AbstractServiceImpl<SystemAccount>
 
     @Override
     @Transactional(rollbackFor = Exception.class)
-    public boolean registerAccount(SystemAccount account){
+    public boolean registerAccount(SystemAccount account) {
         String nickName = account.getNickName();
         String loginName = account.getLoginName();
-
-        List<SystemUser>  existSysUserNames = systemUserMapper.selectSystemUserByName(nickName);
-        if(existSysUserNames.size() > 0)
-            throw new MusicException("昵称[%s]已存在",nickName);
-
+        if (StringUtils.isEmpty(nickName)) {
+            throw new MusicException("未传入昵称");
+        }
+        if (StringUtils.isEmpty(loginName)) {
+            throw new MusicException("未传入账户名");
+        }
+        if (StringUtils.isEmpty(account.getPassword())) {
+            throw new MusicException("未传入密码");
+        }
+        SystemAccount condition = new SystemAccount();
+        condition.setLoginName(loginName);
+        //change 只需要判断账号是否重复即可 使用selectCount即可
+        if (systemAccountMapper.selectCount(new QueryWrapper<>(condition)) > 0) {
+            throw new MusicException("账号名[%s]已存在", loginName);
+        }
         SystemUser user = new SystemUser();
         user.setName(nickName);
-        systemUserService.insertAsync(user);
-
-        List<SystemAccount> existSysLoginNames = systemAccountMapper.selectSystemAccountByName(loginName);
-        if(existSysLoginNames.size() > 0)
-            throw new MusicException("用户登录名[%s]已存在",nickName);
-
+        //change 不调用异步方法
+        systemUserService.insert(user);
         SystemAccount normalizedAccount = new SystemAccount();
         normalizedAccount.setLoginName(loginName);
         normalizedAccount.setNickName(nickName);
 
-        String salt =  RandomUtil.randomString(6);
+        String salt = RandomUtil.randomString(6);
         normalizedAccount.setSalt(salt);
-        normalizedAccount.setPassword(PasswordUtil.encryptPassword(loginName,salt,account.getPassword()));
-
-        insertAsync(normalizedAccount);
-
+        normalizedAccount.setPassword(PasswordUtil.encryptPassword(loginName, salt, account.getPassword()));
+        //change 设置用户id
+        normalizedAccount.setUserId(user.getId());
+        //直接调用mapper.insert
+        mapper().insert(account);
+//        insertAsync(normalizedAccount);
         return true;
     }
-
-    @Override
-    @Async
-    public void insertAsync(SystemAccount account){
-        insert(account);
-    }
+//
+//    @Override
+//    @Async
+//    public void insertAsync(SystemAccount account) {
+//        insert(account);
+//    }
 
 
 }
